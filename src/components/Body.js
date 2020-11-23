06c3e09a54c458adf28c5f26feac8729522bc1f0
Hau Khual <haukhual08@gmail.com>
2020-11-22 23:18:01 -0500

    added react images slider to the project (react-alice-carousel

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,29 +1,14 @@
 import React, { Component } from "react";
-import table from '../images/table_background.jpeg';
-import bible_book from '../images/brown-book-page.jpg';
-import worship from '../images/hands-worship.jpg';
-import church_1 from '../images/church_1.jpg';
-import church_2 from '../images/church_2.jpg';
-import church_3 from '../images/church_3.jpg';
-import worship_lead from '../images/IMG_0100.JPG';
-import Gal_Hiam from '../images/Gal_Hiam.jpg';
-import Cup from '../images/cup.png';
+import Slideshow from './Slideshow.js';
+
 
 class Body extends Component {
   render() {
     return (
-      <div className="body-main">
-        <h2>Imaging sliding</h2>
-        <img src={table} alt="table background" width="700px" height="500px" />
-        <img src={bible_book} alt="Bible Book" width="700px" height="500px" />
-        <img src={worship} alt="Worship" width="700px" height="500px" />
-        <img src={church_1} alt="Church 1" width="700px" height="500px" />
-        <img src={church_2} alt="Church 2" width="700px" height="500px" />
-        <img src={church_3} alt="Church 3" width="700px" height="500px" />
-        <img src={worship_lead} alt="Church 4" width="700px" height="500px" />
-        <img src={Gal_Hiam} alt="book" width="700px" height="500px" />
-        <img src={Cup} alt="cup" width="700px" height="500px" />
+      <div>
+         <Slideshow />
       </div>
+    
     );
   }
 }
--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,5 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+import table from '../images/table_background.jpeg';
+import bible_book from '../images/brown-book-page.jpg';
+import worship from '../images/hands-worship.jpg';
+import church_1 from '../images/church_1.jpg';
+import church_2 from '../images/church_2.jpg';
+import church_3 from '../images/church_3.jpg';
+import worship_lead from '../images/IMG_0100.JPG';
+import Gal_Hiam from '../images/Gal_Hiam.jpg'
+import Cup from '../images/cup.png';
 
-export default function Slideshow() {
-  return <section className="slideshow">Slideshow</section>;
+
+function Slideshow(){
+  return(
+    <div className="slide-show">
+        <AliceCarousel autoPlay autoPlayInterval="1000">
+            <img src={table} alt="cup" width="700px" height="500px" />,
+            <img src={bible_book} alt="cup" width="700px" height="500px" />,
+            <img src={worship} alt="cup" width="700px" height="500px" />,
+            <img src={church_1} alt="cup" width="700px" height="500px" />,
+            <img src={church_2} alt="cup" width="700px" height="500px" />,
+            <img src={church_3} alt="cup" width="700px" height="500px" />,
+            <img src={worship_lead} alt="cup" width="700px" height="500px" />,
+            <img src={Gal_Hiam}  alt="cup" width="700px" height="500px" />,
+            <img src={Cup} alt="cup" width="700px" height="500px" />
+        </AliceCarousel>
+    </div>
+  );
 }
+
+
+export default Slideshow;
