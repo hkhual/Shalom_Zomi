300a0539fef132121f00bd639b53b9993f20c848
Hau Khual <haukhual08@gmail.com>
2020-12-17 19:17:40 -0500

    Updates

---
+++ precache-manifest.ba997c1140ce02e3ef690d78e4f48e13.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10cbeff353f6418ebaae5124cc3f1999",
+    "url": "/Shalom_Zomi/index.html"
+  },
+  {
+    "revision": "d3a6a877a90f3949fac6",
+    "url": "/Shalom_Zomi/static/css/2.cebec226.chunk.css"
+  },
+  {
+    "revision": "63b05c637e8e7d250d5f",
+    "url": "/Shalom_Zomi/static/css/main.34b866e4.chunk.css"
+  },
+  {
+    "revision": "d3a6a877a90f3949fac6",
+    "url": "/Shalom_Zomi/static/js/2.76b5f3ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Shalom_Zomi/static/js/2.76b5f3ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63b05c637e8e7d250d5f",
+    "url": "/Shalom_Zomi/static/js/main.fa788162.chunk.js"
+  },
+  {
+    "revision": "1b339f7faef1cb64d30b",
+    "url": "/Shalom_Zomi/static/js/runtime-main.3cc92df8.js"
+  },
+  {
+    "revision": "fc889811e8633cf58b5ff02eb4eff999",
+    "url": "/Shalom_Zomi/static/media/IMG_0015.fc889811.JPG"
+  },
+  {
+    "revision": "77e75e0fb3e9b5988f09fef24146b4de",
+    "url": "/Shalom_Zomi/static/media/IMG_0017.77e75e0f.JPG"
+  },
+  {
+    "revision": "29cc2ca544313992a6f916fd0eee1836",
+    "url": "/Shalom_Zomi/static/media/IMG_0085.29cc2ca5.JPG"
+  },
+  {
+    "revision": "a6f21e903a29ea5f087639ecb89803da",
+    "url": "/Shalom_Zomi/static/media/IMG_0100.a6f21e90.JPG"
+  },
+  {
+    "revision": "55eaba53869f5e8ce35283665fa0b9fa",
+    "url": "/Shalom_Zomi/static/media/church_1.55eaba53.jpg"
+  },
+  {
+    "revision": "a0347b2535039fe09b36a0717a317b10",
+    "url": "/Shalom_Zomi/static/media/church_4.a0347b25.JPG"
+  },
+  {
+    "revision": "db6a951348e00f5b1e0586fd2f27aa79",
+    "url": "/Shalom_Zomi/static/media/logo512.db6a9513.png"
+  }
+]);
