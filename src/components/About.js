10a2986762146ab74edcc62f2961ecff89174c6e
Hau Khual <haukhual08@gmail.com>
2020-12-23 09:16:14 -0500

    adding new images to slideshow section

--- src/components/About.js
+++ src/components/About.js
@@ -3,10 +3,15 @@ import React, { button } from 'react';
 function About() {
     return (
         <div className="about_main">
+            <div className="about_us">
+                <h2 className="headerTimeService" style={{ fontSize: "1.8em" }}>Our Mission</h2>
+                <p>Love God, Love People, Serve God and serve the World.</p>
+            </div>
             <h2 className="headerTimeService" style={{ fontSize: "1.8em" }}>Service Time and Address</h2>
             <p className="satCircle">Saturday: 7:00PM</p>
             <p className="satCircle">Sunday: 11:30AM</p>
-            <div className="address"><p style={{ fontWeight: 'bold' }}>Location: 7733 Harris Blvd, Charlotte, NC 28212</p></div>
+            <div className="address"><p style={{ fontWeight: 'bold' }}>Location:
+            7733 Harris Blvd, Charlotte, NC 28212</p></div>
         </div>
     );
 }
--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -3,11 +3,12 @@ import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 
 import imag1 from '../images/church_1.jpg';
-import imag2 from '../images/IMG_0015.JPG';
-import imag3 from '../images/IMG_0017.JPG';
-import imag4 from '../images/church_4.JPG';
-import imag5 from '../images/IMG_0100.JPG';
-import imag6 from '../images/IMG_0085.JPG';
+import imag2 from '../images/church_2.jpg'
+import imag3 from '../images/church_4.JPG'
+import imag4 from '../images/IMG_0015.JPG';
+import imag5 from '../images/IMG_0017.JPG';
+import imag6 from '../images/IMG_0100.JPG';
+import imag7 from '../images/IMG_0085.JPG';
 
 
 function Slideshow() {
@@ -20,7 +21,7 @@ function Slideshow() {
         <img src={imag4} alt="" className="slideImage" />
         <img src={imag5} alt="" className="slideImage" />
         <img src={imag6} alt="" className="slideImage" />
-
+        <img src={imag7} alt="" className="slideImage" />
 
       </AliceCarousel>
     </div>
